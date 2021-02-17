202d4f74e298677b92e9a74e01462accadc44ada
panicoli0 <pablonicoli21@gmail.com>
2021-02-17 18:28:56 -0300

    Initial commit

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SplashScreen : MonoBehaviour
+{
+
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] int level;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(level);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Proto-12-Argon-Assault
+ Rail Space Shooter Game
